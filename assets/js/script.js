26753496fdd9ce0423edf2bf7c74c0c632c9f296
pippo98s <filippo5scarcella@gmail.com>
2019-11-12 16:36:21 +0100

    bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var message = $(".footer input").val();
     console.log(message);
     
-    var element = $(".user-sms").clone();
+    var element = $(".user-sms:first-child").clone();
     console.log(element);
 
     var output = element.html("<p>"+ message + "</p>");
