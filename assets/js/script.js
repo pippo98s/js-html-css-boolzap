b88c52c70be15397ae595a29a782afd974246a5e
pippo98s <filippo5scarcella@gmail.com>
2019-11-13 15:10:22 +0100

    add a generic reply

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
 
   $("#send").click(function(){
     var message = $(".footer input").val();
-    if (message.length > 0 && !/^\s+$/.test(message)) {  // !/^\s+$/.test() checks white spaces
-      var element = $(".user-sms:first-child").clone();
-      var output = element.html("<p>" + message + "</p>");
-      $(".chat").append(output);
+    if (message.length > 0 && !/^\s+$/.test(message)) {  // !/^\s+$/.test() checks there are only white spaces
+      var element = $(".template .user-sms").clone();
+      element.html("<p>" + message + "</p>")
+      $(".chat").append(element);
+
+      // risposta automatica dopo un secondo
+      setTimeout(function(){
+        var smsDefault = "ok";
+        var pc = $(".template .pc-sms").clone();
+        pc.html("<p>" + smsDefault + "</p>");
+        $(".chat").append(pc);
+        
+      } , 1000);
     }
     $(".footer input").val("");
   });
